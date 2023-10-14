c34020a05cfc2a3ab2d2ff974f3423f5618cf34e
jay.yang <858851768@qq.com>
2023-10-14 11:55:26 +0800

    🎈 perf: eslint and prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import './App.css';
 import { MenuCard } from './components/MenuCard';
 
 function App() {
-  return (
-    <div className="App">
-      <MenuCard title='INPUT' />
-    </div>
-  );
+    return (
+        <div className="App">
+            <MenuCard title="INPUT" />
+        </div>
+    );
 }
 
 export default App;
