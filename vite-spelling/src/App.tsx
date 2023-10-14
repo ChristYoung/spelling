d613308c979f24d766e29618d29292ec8aea4ff1
jay.yang <858851768@qq.com>
2023-10-14 16:25:14 +0800

    ✨ feat: 使用tailwindcss.

--- vite-spelling/src/App.tsx
+++ vite-spelling/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
     return (
         <>
-            <p className="underline text-3xl">
+            <p className="underline text-lg hover:bg-red-500">
                 Click on the Vite and React logos to learn more
             </p>
         </>
--- vite-spelling/tailwind.config.js
+++ vite-spelling/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: [ './index.html', './src/**/*.{ts,tsx}' ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-light': '#F0F0F0',
+        'primary-dark': '#1B1A22',
+      }
+    },
   },
   plugins: [],
 }
