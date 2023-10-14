555f5f35fdc62624f9b97c8e5cfce602b17b4a38
jay.yang <858851768@qq.com>
2023-10-14 15:48:12 +0800

    🌈 style: init tailwindcss.

---
+++ vite-spelling/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <p className="underline text-3xl">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
+}
+
+export default App;
