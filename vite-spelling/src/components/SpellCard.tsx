31bcf58d2b001648dd9ef2bb25a11d701ea22def
jay.yang <858851768@qq.com>
2023-10-22 17:31:25 +0800

    🌈 style:

--- vite-spelling/src/components/SpellCard.tsx
+++ vite-spelling/src/components/SpellCard.tsx
@@ -34,72 +34,50 @@ export const SpellCard: React.FC<WordsItem> = (props: WordsItem) => {
     return (
         <>
             <InputHandler updateInput={updateInput}></InputHandler>
-            <div className="container mx-auto flex h-full flex-1 flex-col items-center justify-center pb-5">
-                <div className="container relative mx-auto flex h-full flex-col items-center">
-                    <div className="container flex flex-grow items-center justify-center">
-                        <div className="container flex h-full w-full flex-col items-center justify-center">
-                            <div className="container flex h-24 w-full shrink-0 grow-0 justify-between px-12 pt-10"></div>
-                            <div className="container flex flex-grow flex-col items-center justify-center">
-                                <div className="relative flex w-full justify-center">
-                                    <div className="relative">
-                                        <div
-                                            lang="en"
-                                            className="flex flex-col items-center justify-center pb-1 pt-4">
-                                            <div className="tooltip-info relative w-fit bg-transparent p-0 leading-normal shadow-none dark:bg-transparent tooltip">
-                                                <div className="flex items-center false justify-center ">
-                                                    {word
-                                                        .split('')
-                                                        .map((w, _index) => {
-                                                            return (
-                                                                <span
-                                                                    key={`${_index}_${w}_word`}
-                                                                    className={`m-0 p-0 font-mono text-[48px] px-1 font-normal  dark:text-gray-50 duration-0 dark:text-opacity-80 ${
-                                                                        w ===
-                                                                        displayWords[
-                                                                            _index
-                                                                        ]
-                                                                            ? SpellingStateClassNames.correct
-                                                                            : SpellingStateClassNames.normal
-                                                                    }`}>
-                                                                    {displayWords[
-                                                                        _index
-                                                                    ]
-                                                                        ? displayWords[
-                                                                              _index
-                                                                          ]
-                                                                        : '_'}
-                                                                </span>
-                                                            );
-                                                        })}
-                                                </div>
-                                                <button
-                                                    type="button"
-                                                    className="focus:outline-none dark:fill-gray-400 dark:opacity-80 cursor-pointer text-gray-600 absolute -right-12 top-1/2 h-9 w-9 -translate-y-1/2 transform ">
-                                                    <HornIcon />
-                                                </button>
-                                            </div>
-                                        </div>
-                                        {/* TODO: 后续看有没有API可以获取到音标. */}
-                                        {/* <div className="space-x-5 text-center text-sm font-normal text-gray-600 transition-colors duration-300 dark:text-gray-400 false">
+            <div className="relative">
+                <div
+                    lang="en"
+                    className="flex flex-col items-center justify-center pb-1 pt-4">
+                    <div className="tooltip-info relative w-fit bg-transparent p-0 leading-normal shadow-none dark:bg-transparent tooltip">
+                        <div className="flex items-center false justify-center ">
+                            {word.split('').map((w, _index) => {
+                                return (
+                                    <span
+                                        key={`${_index}_${w}_word`}
+                                        className={`m-0 p-0 font-mono text-[48px] px-1 font-normal  dark:text-gray-50 duration-0 dark:text-opacity-80 ${
+                                            w === displayWords[_index]
+                                                ? SpellingStateClassNames.correct
+                                                : SpellingStateClassNames.normal
+                                        }`}>
+                                        {displayWords[_index]
+                                            ? displayWords[_index]
+                                            : '_'}
+                                    </span>
+                                );
+                            })}
+                        </div>
+                        <button
+                            type="button"
+                            className="focus:outline-none dark:fill-gray-400 dark:opacity-80 cursor-pointer text-gray-600 absolute -right-12 top-1/2 h-9 w-9 -translate-y-1/2 transform ">
+                            <HornIcon />
+                        </button>
+                    </div>
+                </div>
+                {/* TODO: 后续看有没有API可以获取到音标. */}
+                {/* <div className="space-x-5 text-center text-sm font-normal text-gray-600 transition-colors duration-300 dark:text-gray-400 false">
                                             <span>
                                                 AmE: [ɪk'splosɪv; ɪk'splozɪv]
                                             </span>
                                         </div> */}
-                                        {/* 中文释义 */}
-                                        <div className="flex items-center justify-center  pb-4 pt-5">
-                                            <span
-                                                className="max-w-4xl text-center font-sans transition-colors duration-300 dark:text-white dark:text-opacity-80 false false"
-                                                style={{
-                                                    fontSize: '18px',
-                                                }}>
-                                                爆炸的； 极易引起争论的；炸药
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                {/* 中文释义 */}
+                <div className="flex items-center justify-center  pb-4 pt-5">
+                    <span
+                        className="max-w-4xl text-center font-sans transition-colors duration-300 dark:text-white dark:text-opacity-80 false false"
+                        style={{
+                            fontSize: '18px',
+                        }}>
+                        爆炸的； 极易引起争论的；炸药
+                    </span>
                 </div>
             </div>
         </>
--- vite-spelling/src/pages/SpellWords.tsx
+++ vite-spelling/src/pages/SpellWords.tsx
@@ -2,10 +2,22 @@ import { SpellCard } from '../components/SpellCard';
 
 export const SpellWords: React.FC = () => {
     return (
-        <div className="__SpellWords">
-            <SpellCard
-                word="age"
-                familiar={false}></SpellCard>
+        <div className="container mx-auto flex h-full flex-1 flex-col items-center justify-center pb-5">
+            <div className="container relative mx-auto flex h-full flex-col items-center">
+                <div className="container flex flex-grow items-center justify-center">
+                    <div className="container flex h-full w-full flex-col items-center justify-center">
+                        <div className="container flex h-24 w-full shrink-0 grow-0 justify-between px-12 pt-10"></div>
+                        <div className="container flex flex-grow flex-col items-center justify-center">
+                            <div className="relative flex w-full justify-center">
+                                <SpellCard
+                                    word="remote"
+                                    familiar={false}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
