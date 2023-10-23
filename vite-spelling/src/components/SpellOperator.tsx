08bc0e6e2822b83228653dd83433e2f4b7bdbdcd
jay.yang <858851768@qq.com>
2023-10-23 22:22:26 +0800

    🐳 chore: update.

--- vite-spelling/src/components/SpellOperator.tsx
+++ vite-spelling/src/components/SpellOperator.tsx
@@ -23,21 +23,27 @@ export const SpellOperator: React.FC = (props: { familiar: boolean }) => {
                     '0 100px 80px #322e8112, 0 41.7776px 33.4221px #322e810d, 0 22.3363px 17.869px #322e810b, 0 12.5216px 10.0172px #322e8109, 0 6.6501px 5.32008px #322e8107, 0 2.76726px 2.21381px #322e8105',
             }}
             className="__SpellOperator text-3xl flex w-3/5 rounded-xl bg-white p-4 py-10 opacity-50 transition-colors duration-300 dark:bg-gray-800">
-            <div className="flex flex-1 flex-col items-center justify-center cursor-pointer">
-                <span
-                    className="w-4/5 pb-2 text-center font-bold text-gray-600 transition-colors duration-300 dark:text-gray-400"
-                    onClick={() => {
-                        if (currentWordIndex + 1 > 0) {
-                            dispatch(
-                                changeCurrentWordByIndex(currentWordIndex - 1),
-                            );
-                        }
-                    }}>
+            <div
+                className="flex flex-1 flex-col items-center justify-center cursor-pointer"
+                onClick={() => {
+                    if (currentWordIndex + 1 > 0) {
+                        dispatch(
+                            changeCurrentWordByIndex(currentWordIndex - 1),
+                        );
+                    }
+                }}>
+                <span className="w-4/5 pb-2 text-center font-bold text-gray-600 transition-colors duration-300 dark:text-gray-400">
                     <AiOutlineArrowLeft style={{ display: 'inline' }} />
                 </span>
             </div>
             <div className="flex flex-1 flex-col items-center justify-center cursor-pointer">
-                <span className="w-4/5 pb-2 text-center font-bold text-gray-600 transition-colors duration-300 dark:text-gray-400">
+                <span
+                    className="w-4/5 pb-2 text-center font-bold text-gray-600 transition-colors duration-300 dark:text-gray-400"
+                    title={
+                        familiar
+                            ? 'Removal of the "mastered" marker.'
+                            : 'Marked as mastered.'
+                    }>
                     {familiar ? (
                         <AiTwotoneHeart style={{ display: 'inline' }} />
                     ) : (
