77fb6440495b16f5ec1774316e67014f29136277
jay.yang <858851768@qq.com>
2023-10-28 19:15:28 +0800

    🦄 refactor: createHashRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { RootLayout } from '../pages/RootLayout';
+import { createHashRouter } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { InputWords } from '../pages/InputWords';
-import { ViewWords } from '../pages/ViewWords';
+import { RootLayout } from '../pages/RootLayout';
 import { SpellWords } from '../pages/SpellWords';
+import { ViewWords } from '../pages/ViewWords';
 
-const routerConfig = createBrowserRouter([
+const routerConfig = createHashRouter([
     {
         path: '/',
         element: <RootLayout />,
