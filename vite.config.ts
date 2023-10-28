1962feaa15f1caa80bb2817335a960b086d217a6
jay.yang <858851768@qq.com>
2023-10-28 17:01:33 +0800

    🔧 build: set base by the envirmonet.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: process.env.NODE_ENV === 'production' ? 'spelling' : '/',
     build: {
         rollupOptions: {
             output: {
