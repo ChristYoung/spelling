58efe726d55b8c777cf23cdc284e072602a6e786
jay.yang <858851768@qq.com>
2023-10-30 15:05:26 +0800

    🦄 refactor:

--- src/types/filter.type.ts
+++ src/types/filter.type.ts
@@ -2,6 +2,6 @@ export interface FilterWordsType {
     // range?: [number, number]; // Tuple Type.
     startRange?: number;
     endRange?: number;
-    timeFilter?: 'last_7d' | 'last_30d' | 'last_90d';
+    timeFilter?: -7 | -30 | -90;
     familiarFilter?: 'all' | 'only_familiar' | 'only_unfamiliar';
 }
--- src/utils/date.util.ts
+++ src/utils/date.util.ts
@@ -1,3 +1,7 @@
 export function getDateString(): number {
     return new Date().getTime();
 }
+
+export function getXDaysAgoOrAfterTime(x: number): number {
+    return new Date(new Date().getTime() + x * 24 * 60 * 60 * 1000).getTime();
+}
